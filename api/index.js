484142126e58cca3d4972a9909809af624c61036
Thiago Lima <thiagolima.unb@gmail.com>
2019-08-09 22:59:07 -0300

    Add api setup

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const Joi = require("@hapi/joi");
+Joi.objectId = require("joi-objectid")(Joi);
+const mongoose = require("mongoose");
+const cors = require("cors");
+const express = require("express");
+const users = require("./routes/users");
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/users", users);
+app.get("/", (req, res) => {
+  res.send("Home");
+});
+
+// Connect to the database
+mongoose
+  .connect("mongodb://localhost:27017/aerolito", { useNewUrlParser: true })
+  .then("Connected to the database.")
+  .catch(error => console.log("Could not connect to the database!", error));
+
+app.listen(5000, console.log("Backend listening on port 5000..."));
