3d75386ce094524f702c1314a9c042fdc9a86243
Marcelo Cristiano <marcelo@lemon.energy>
2019-11-11 00:52:40 -0300

    Mock data

---
+++ client/src/mock/chapter.js
@@ -0,0 +1,29 @@
+export default {
+  nextHQ: 2,
+  images: [
+    {
+      id: 1,
+      img: "http://placekitten.com/g/500/500"
+    },
+    {
+      id: 2,
+      img: "http://placekitten.com/g/501/500"
+    },
+    {
+      id: 3,
+      img: "http://placekitten.com/g/502/500"
+    },
+    {
+      id: 4,
+      img: "http://placekitten.com/g/503/500"
+    },
+    {
+      id: 5,
+      img: "http://placekitten.com/g/504/500"
+    },
+    {
+      id: 6,
+      img: "http://placekitten.com/g/505/500"
+    }
+  ]
+};
---
+++ client/src/mock/chapters.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1,
+    src: "http://placekitten.com/g/500/500"
+  },
+  {
+    id: 2,
+    src: "http://placekitten.com/g/501/500"
+  },
+  {
+    id: 3,
+    src: "http://placekitten.com/g/502/500"
+  },
+  {
+    id: 4,
+    src: "http://placekitten.com/g/503/500"
+  }
+];
