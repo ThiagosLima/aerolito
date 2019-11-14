e1d3f5064149ee3bda4ca26decc0990255606c41
Marcelo Cristiano <marcelo@lemon.energy>
2019-11-13 22:56:25 -0300

    Working on header styling and applying BEM

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -11,29 +11,57 @@ const MainNavBar = () => {
           <Row>
             <Col xs={12} md={4}>
               <Link to="/series">
-                <div className="header--logo">
+                <div className="header__logo">
                   <img src={logo} alt="Meteoro logo do site" />
                 </div>
-                <div className="header--circle" />
+                <div className="header__circle" />
               </Link>
             </Col>
             <Col align="end" xs={12} md={2} push={{ md: 6 }}>
               O O O
             </Col>
             <Col xs={12} md={6} pull={{ md: 2 }}>
-              <nav className="header--nav">
-                <ul>
-                  <li>
-                    <NavLink className="header--nav__item header--nav__item-border" to="/series">Início</NavLink>
+              <nav className="header__nav">
+                <ul className="header__ul">
+                  <li className="header__li">
+                    <NavLink
+                      className="header__item--active"
+                      to="/series"
+                    >
+                      <span className="header__link header__link--border">
+                        Início
+                      </span>
+                    </NavLink>
                   </li>
-                  <li>
-                    <NavLink className="header--nav__item header--nav__item-border"  to="/shop">Comprar</NavLink>
+                  <li className="header__li">
+                    <NavLink
+                      className="header__item--active"
+                      to="/shop"
+                    >
+                      <span className="header__link header__link--border">
+                        Comprar
+                      </span>
+                    </NavLink>
                   </li>
-                  <li>
-                    <NavLink className="header--nav__item header--nav__item-border"  to="/credits">Créditos</NavLink>
+                  <li className="header__li">
+                    <NavLink
+                      className="header__item--active"
+                      to="/credits"
+                    >
+                      <span className="header__link header__link--border">
+                        Créditos
+                      </span>
+                    </NavLink>
                   </li>
-                  <li>
-                    <NavLink className="header--nav__item"  to="/contact">Contato</NavLink>
+                  <li className="header__li">
+                    <NavLink
+                      className="header__item--active"
+                      to="/contact"
+                    >
+                      <span className="header__link">
+                        Contato
+                      </span>
+                    </NavLink>
                   </li>
                 </ul>
               </nav>
@@ -41,7 +69,7 @@ const MainNavBar = () => {
           </Row>
         </Container>
       </Container>
-      <div className="header--full-width-row" />
+      <div className="header__full-width-row" />
     </header>
   );
 };
