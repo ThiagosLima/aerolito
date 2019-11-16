81423d243bd743f45b5067e7dd342628c41c1c06
Marcelo Cristiano <marcelo@lemon.energy>
2019-11-16 01:09:00 -0300

    Header desktop withousocial media buttons

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import { Container, Row, Col } from "react-grid-system";
+import { Container, Row, Col, Visible } from "react-grid-system";
 import logo from "../logo.png";
+import br from "../assets/img/Br.png";
+import uk from "../assets/img/Uk.png";
 
 const MainNavBar = () => {
   return (
-    <header className="header">
-      <Container fluid>
+    <header>
+      <Container fluid className="header">
         <Container>
           <Row>
-            <Col xs={12} md={4}>
+            <Col xs={12} md={3}>
               <Link to="/series">
                 <div className="header__logo">
                   <img src={logo} alt="Meteoro logo do site" />
@@ -17,54 +19,72 @@ const MainNavBar = () => {
                 <div className="header__circle" />
               </Link>
             </Col>
-            <Col align="end" xs={12} md={2} push={{ md: 6 }}>
+            <Col align="end" xs={12} md={1} push={{ md: 8 }}>
               O O O
             </Col>
-            <Col xs={12} md={6} pull={{ md: 2 }}>
-              <nav className="header__nav">
-                <ul className="header__ul">
-                  <li className="header__li">
-                    <NavLink
-                      className="header__item--active"
-                      to="/series"
-                    >
-                      <span className="header__link header__link--border">
-                        Início
-                      </span>
-                    </NavLink>
-                  </li>
-                  <li className="header__li">
-                    <NavLink
-                      className="header__item--active"
-                      to="/shop"
-                    >
-                      <span className="header__link header__link--border">
-                        Comprar
-                      </span>
-                    </NavLink>
-                  </li>
-                  <li className="header__li">
-                    <NavLink
-                      className="header__item--active"
-                      to="/credits"
-                    >
-                      <span className="header__link header__link--border">
-                        Créditos
-                      </span>
-                    </NavLink>
-                  </li>
-                  <li className="header__li">
-                    <NavLink
-                      className="header__item--active"
-                      to="/contact"
-                    >
-                      <span className="header__link">
-                        Contato
-                      </span>
-                    </NavLink>
-                  </li>
-                </ul>
-              </nav>
+            <Col xs={12} md={8} pull={{ md: 1 }}>
+              <Row>
+                <Col xs={2}>
+                  <div className="header__flags-container">
+                    <img
+                      className="header__flag"
+                      src={br}
+                      alt="Bandeira do Brasil"
+                    ></img>
+                    <img
+                      className="header__flag"
+                      src={uk}
+                      alt="Bandeira do Reino Unido"
+                    ></img>
+                  </div>
+                </Col>
+                <Col xs={8}>
+                  <nav className="header__nav">
+                    <ul className="header__ul">
+                      <li className="header__li">
+                        <NavLink
+                          className="header__item header__item--active"
+                          to="/series"
+                        >
+                          <span className="header__link header__link--active header__link--border">
+                            Início
+                          </span>
+                        </NavLink>
+                      </li>
+                      <li className="header__li">
+                        <NavLink
+                          className="header__item header__item--active"
+                          to="/shop"
+                        >
+                          <span className="header__link header__link--active header__link--border">
+                            Comprar
+                          </span>
+                        </NavLink>
+                      </li>
+                      <li className="header__li">
+                        <NavLink
+                          className="header__item header__item--active"
+                          to="/credits"
+                        >
+                          <span className="header__link header__link--active header__link--border">
+                            Créditos
+                          </span>
+                        </NavLink>
+                      </li>
+                      <li className="header__li">
+                        <NavLink
+                          className="header__item header__item--active"
+                          to="/contact"
+                        >
+                          <span className="header__link header__link--active">
+                            Contato
+                          </span>
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </nav>
+                </Col>
+              </Row>
             </Col>
           </Row>
         </Container>
