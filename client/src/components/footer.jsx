0c7c35a3d447c78864f7fd0f4aea8094e9b82a96
Marcelo Cristiano <marcelo@lemon.energy>
2019-12-29 22:48:40 -0300

    Finishing footer

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import fac from "../assets/img/fac_logo.png";
+import secretaria from "../assets/img/secretaria_logo.png"
 
 const Footer = () => {
   return (
@@ -6,6 +8,10 @@ const Footer = () => {
       <div className="footer__sponsor">
         Este projeto foi realizado com recursos do Fundo de Apoio à Cultura do
         Distrito Federal
+      </div>
+      <div className="footer__imgs">
+        <img className="footer__fac-img" alt="Fac logo" src={fac} />
+        <img className="footer__sec-img" alt="Fac logo" src={secretaria} />
       </div>
       <div className="footer__copyright">
         Aerolito © 2019. Todos os direitos reservados. Desenvolvido por
