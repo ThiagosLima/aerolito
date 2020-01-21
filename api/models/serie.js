3019b7892e2fd0c196eb66a1b5ecae0190403bf6
Thiago Lima <thiagolima.unb@gmail.com>
2020-01-21 00:11:55 -0300

    Add serie model

---
+++ api/models/serie.js
@@ -0,0 +1,57 @@
+const Joi = require("@hapi/joi");
+const mongoose = require("mongoose");
+
+const serieSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  authors: {
+    type: String,
+    required: true
+  },
+  drawings: {
+    type: String,
+    required: true
+  },
+  colors: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: Number,
+    required: true
+  },
+  pages: {
+    type: Number,
+    required: true
+  },
+  synopsis: {
+    type: String,
+    required: true
+  }
+});
+
+const Serie = mongoose.model("Serie", serieSchema);
+
+function validate(serie) {
+  const schema = {
+    title: Joi.string().required(),
+    authors: Joi.string().required(),
+    drawings: Joi.string().required(),
+    colors: Joi.string().required(),
+    genre: Joi.string().required(),
+    year: Joi.number().required(),
+    pages: Joi.number().required(),
+    synopsis: Joi.string().required()
+  };
+
+  return Joi.validate(serie, schema);
+}
+
+module.exports = { serieSchema, Serie, validate };
