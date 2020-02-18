da7626d7f5f021b06a485b1e65a03053fb429d94
Thiago Lima <thiagolima.unb@gmail.com>
2020-02-18 10:42:30 -0300

    Add getPages service

--- client/src/services/chapterService.js
+++ client/src/services/chapterService.js
@@ -3,25 +3,26 @@ import http from "./httpService";
 const apiEndpoint = "http://localhost:4000/api/chapters";
 const awsEndpint = `https://aerolito-teste1.s3-sa-east-1.amazonaws.com`;
 
-export async function getChapters(serieId, serieAwsId) {
+export async function getChapters(serieId) {
   let { data: chapters } = await http.get(`${apiEndpoint}/serie/${serieId}`);
-  console.log(chapters);
 
   // Update image url
   chapters.forEach(chapter => {
-    chapter.cover = `${awsEndpint}/${serieAwsId}/${chapter.awsId}/${chapter.cover}`;
+    chapter.cover = `${awsEndpint}/${chapter.awsSerieId}/${chapter.awsId}/${chapter.cover}`;
   });
 
   return chapters;
 }
 
-export async function getChapter(id) {
+export async function getPages(id) {
   let { data: chapter } = await http.get(`${apiEndpoint}/${id}`);
 
   // Update image url
-  chapter.pages.forEach(page => {
-    chapter.cover = `${awsEndpint}/${chapter.awsId}/${chapter.cover}`;
-  });
+  for (let i = 0; i < chapter.pages.length; i++) {
+    chapter.pages[
+      i
+    ] = `${awsEndpint}/${chapter.awsSerieId}/${chapter.awsId}/${chapter.pages[i]}`;
+  }
 
-  return chapter;
+  return chapter.pages;
 }
