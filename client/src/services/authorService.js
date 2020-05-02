53313b151d0207d80d28fcc6b17aad64ec92c0fe
Thiago Lima <thiagolima.unb@gmail.com>
2020-05-02 00:26:18 -0300

    Add getAuthors service

--- client/src/services/authorService.js
+++ client/src/services/authorService.js
@@ -5,3 +5,7 @@ const apiEndpoint = "http://localhost:4000/api/authors";
 export function createAuthor(data) {
   return http.post(apiEndpoint, { ...data });
 }
+
+export function getAuthors() {
+  return http.get(apiEndpoint);
+}
