bd1ced230f7f8fcc0d7ce7f302d62bab7b6ea71d
Thiago Lima <thiagolima.unb@gmail.com>
2020-05-17 10:54:04 -0300

    Show the name of the uploading file

--- client/src/components/chapterForm.jsx
+++ client/src/components/chapterForm.jsx
@@ -1,63 +1,63 @@
-import React from "react";
-import Joi from "joi-browser";
-import { Container } from "react-grid-system";
-import Form from "./common/form";
-import chapterService from "../services/chapterService";
-import awsService from "../services/awsService";
-import serieService from "../services/serieService";
+import React from 'react'
+import Joi from 'joi-browser'
+import { Container } from 'react-grid-system'
+import Form from './common/form'
+import chapterService from '../services/chapterService'
+import awsService from '../services/awsService'
+import serieService from '../services/serieService'
 
 class ChapterForm extends Form {
   state = {
     serie: {},
-    file: "",
-    files: "",
+    file: '',
+    files: '',
     data: {
-      title: "",
-      number: ""
+      title: '',
+      number: ''
     },
     errors: {}
-  };
+  }
 
   schema = {
-    title: Joi.string().required().label("Título"),
-    number: Joi.number().required().label("Número")
-  };
+    title: Joi.string().required().label('Título'),
+    number: Joi.number().required().label('Número')
+  }
 
   async componentDidMount() {
-    const serieId = this.props.match.params.id;
-    const serie = await serieService.getSerie(serieId);
-    this.setState({ serie });
+    const serieId = this.props.match.params.id
+    const serie = await serieService.getSerie(serieId)
+    this.setState({ serie })
   }
 
   awsCoverUpload = async () => {
-    const { file, serie } = this.state;
+    const { file, serie } = this.state
 
     const { url, awsId, key: cover } = await awsService.getCoverConfig(
       file,
       serie.awsId
-    );
+    )
 
-    await awsService.putFile(url, file);
+    await awsService.putFile(url, file, this.handleProgressBar)
 
-    return { awsId, cover };
-  };
+    return { awsId, cover }
+  }
 
   awsPagesUpload = async (awsId, cover) => {
-    const { files, data, serie } = this.state;
-    let pages = [];
+    const { files, data, serie } = this.state
+    let pages = []
 
     for (const file of [...files]) {
       const pageData = {
         awsSerieId: serie.awsId,
         awsChapterId: awsId,
         file
-      };
+      }
 
-      const { url, name: page } = await awsService.getPageConfig(pageData);
+      const { url, name: page } = await awsService.getPageConfig(pageData)
 
-      await awsService.putFile(url, file);
+      await awsService.putFile(url, file, this.handleProgressBar)
 
-      pages.push(page);
+      pages.push(page)
     }
 
     return {
@@ -67,24 +67,24 @@ class ChapterForm extends Form {
       pages,
       serieId: serie._id,
       awsSerieId: serie.awsId
-    };
-  };
+    }
+  }
 
   doSubmit = async () => {
     try {
-      const { awsId, cover } = await this.awsCoverUpload();
-      const chapter = await this.awsPagesUpload(awsId, cover);
+      const { awsId, cover } = await this.awsCoverUpload()
+      const chapter = await this.awsPagesUpload(awsId, cover)
 
-      await chapterService.postChapter(chapter);
+      await chapterService.postChapter(chapter)
 
-      window.location = "/";
+      window.location = '/'
     } catch (error) {
       if (error.response && error.response.status === 400) {
-        const errors = { ...this.state.errors };
-        this.setState({ errors });
+        const errors = { ...this.state.errors }
+        this.setState({ errors })
       }
     }
-  };
+  }
 
   render() {
     return (
@@ -92,16 +92,17 @@ class ChapterForm extends Form {
         <section className="section section--light">
           <h1>Cadastrar capítulo</h1>
           <form onSubmit={this.handleSubmit} className="form">
-            {this.renderFileInput("Capa", "image/*")}
-            {this.renderFileInput("Páginas", "image/*", true)}
-            {this.renderInput("title", "Título")}
-            {this.renderInput("number", "Número do capítulo")}
-            {this.renderButton("Cadastrar")}
+            {this.renderFileInput('Capa', 'image/*')}
+            {this.renderFileInput('Páginas', 'image/*', true)}
+            {this.renderInput('title', 'Título')}
+            {this.renderInput('number', 'Número do capítulo')}
+            {this.renderProgressBar()}
+            {this.renderButton('Cadastrar')}
           </form>
         </section>
       </Container>
-    );
+    )
   }
 }
 
-export default ChapterForm;
+export default ChapterForm
--- client/src/components/serieForm.jsx
+++ client/src/components/serieForm.jsx
@@ -1,61 +1,61 @@
-import React from "react";
-import Joi from "joi-browser";
-import Form from "./common/form";
-import serieService from "../services/serieService";
-import awsService from "../services/awsService";
-import { Container } from "react-grid-system";
+import React from 'react'
+import Joi from 'joi-browser'
+import Form from './common/form'
+import serieService from '../services/serieService'
+import awsService from '../services/awsService'
+import { Container } from 'react-grid-system'
 
 class SerieForm extends Form {
   state = {
-    file: "",
+    file: '',
     data: {
-      title: "",
-      authors: "",
-      drawings: "",
-      colors: "",
-      genre: "",
-      year: "",
-      pages: "",
-      synopsis: ""
+      title: '',
+      authors: '',
+      drawings: '',
+      colors: '',
+      genre: '',
+      year: '',
+      pages: '',
+      synopsis: ''
     },
     errors: {}
-  };
+  }
 
   schema = {
-    title: Joi.string().required().label("Título"),
-    authors: Joi.string().required().label("Autores"),
-    drawings: Joi.string().required().label("Desenhos"),
-    colors: Joi.string().required().label("Cores"),
-    genre: Joi.string().required().label("Gênero"),
-    year: Joi.number().required().label("Ano"),
-    pages: Joi.number().required().label("Páginas"),
-    synopsis: Joi.string().required().label("Sinopse")
-  };
+    title: Joi.string().required().label('Título'),
+    authors: Joi.string().required().label('Autores'),
+    drawings: Joi.string().required().label('Desenhos'),
+    colors: Joi.string().required().label('Cores'),
+    genre: Joi.string().required().label('Gênero'),
+    year: Joi.number().required().label('Ano'),
+    pages: Joi.number().required().label('Páginas'),
+    synopsis: Joi.string().required().label('Sinopse')
+  }
 
   awsUpload = async () => {
-    const { file, data } = this.state;
+    const { file, data } = this.state
 
-    const { url, awsId, key: cover } = await awsService.getCoverConfig(file);
-    await awsService.putFile(url, file, this.uploadPercentage);
+    const { url, awsId, key: cover } = await awsService.getCoverConfig(file)
+    await awsService.putFile(url, file, this.handleProgressBar)
 
-    return { cover, awsId, ...data };
-  };
+    return { cover, awsId, ...data }
+  }
 
   doSubmit = async () => {
     try {
-      const serie = await this.awsUpload();
+      const serie = await this.awsUpload()
 
-      await serieService.postSerie(serie);
+      await serieService.postSerie(serie)
 
-      window.location = "/";
+      window.location = '/'
     } catch (error) {
       if (error.response && error.response.status === 400) {
-        const errors = { ...this.state.errors };
-        errors.email = error.response.data;
-        this.setState({ errors });
+        const errors = { ...this.state.errors }
+        errors.email = error.response.data
+        this.setState({ errors })
       }
     }
-  };
+  }
 
   render() {
     return (
@@ -63,22 +63,22 @@ class SerieForm extends Form {
         <section className="section section--light">
           <h1>Cadastrar série</h1>
           <form onSubmit={this.handleSubmit} className="form">
-            {this.renderFileInput("Capa", "image/*")}
-            {this.renderInput("title", "Título")}
-            {this.renderInput("authors", "Autores")}
-            {this.renderInput("drawings", "Desenhos")}
-            {this.renderInput("colors", "Cores")}
-            {this.renderInput("genre", "Gênero")}
-            {this.renderInput("year", "Ano", "number")}
-            {this.renderInput("pages", "Páginas", "number")}
-            {this.renderTextArea("synopsis", "Sinopse", 10)}
+            {this.renderFileInput('Capa', 'image/*')}
+            {this.renderInput('title', 'Título')}
+            {this.renderInput('authors', 'Autores')}
+            {this.renderInput('drawings', 'Desenhos')}
+            {this.renderInput('colors', 'Cores')}
+            {this.renderInput('genre', 'Gênero')}
+            {this.renderInput('year', 'Ano', 'number')}
+            {this.renderInput('pages', 'Páginas', 'number')}
+            {this.renderTextArea('synopsis', 'Sinopse', 10)}
             {this.renderProgressBar()}
-            {this.renderButton("Cadastrar")}
+            {this.renderButton('Cadastrar')}
           </form>
         </section>
       </Container>
-    );
+    )
   }
 }
 
-export default SerieForm;
+export default SerieForm
